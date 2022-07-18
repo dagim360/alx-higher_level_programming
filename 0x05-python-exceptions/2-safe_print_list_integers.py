fef8dc09fba6bc590b3acfba4b45f17a89ae04a9
dagim360 <dagim360@gmail.com>
2022-07-18 04:28:50 -0700

    cat

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+    print("")
+    return j
